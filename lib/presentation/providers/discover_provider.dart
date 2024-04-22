2d017716c995b135ed8a8cc60511fd07efffb673
Davichobits <davidruizdeveloper@gmail.com>
2024-04-22 18:53:16 -0500

    Map implemented inside provider

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/widgets.dart';
 import 'package:toktik_app/domain/entities/video_posts.dart';
+import 'package:toktik_app/infrastructure/models/local_video_model.dart';
+import 'package:toktik_app/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  //TODO: Repository, DataSource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    //TODO: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
