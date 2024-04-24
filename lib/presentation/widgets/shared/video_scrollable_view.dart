814f9f5cb8e09942647b1f317601d2ffc0e77271
Davichobits <davidruizdeveloper@gmail.com>
2024-04-24 12:50:56 -0500

    Icons with value on bottom

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:toktik_app/domain/entities/video_posts.dart';
+import 'package:toktik_app/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
 
-  const VideoScrollableView({
-    super.key,
-    required this.videos
-  });
+  const VideoScrollableView({super.key, required this.videos});
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
-      physics: const BouncingScrollPhysics(), 
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue)
-      ],
+      physics: const BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            // Video Player + gradiente
+            // Botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+            ),
+          ],
+        );
+      },
     );
   }
 }
